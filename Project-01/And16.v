2834272f9844e01477db34a1c33f9e2ae4502bcb
Poojan-84 <viranipoojan2@gmail.com>
2025-12-18 10:19:58 +0530

    Add 16-bit AND gate module in And16.v

---
+++ Project-01/And16.v
@@ -0,0 +1,9 @@
+module and16 (input wire [15:0] a, input wire [15:0] b, output wire [15:0] c);
+  genvar i;
+  generate
+    for(i=0;i<16;i = i+1) begin
+      assign c[i]=a[i]&b[i];
+    end
+  endgenerate
+endmodule
+    
